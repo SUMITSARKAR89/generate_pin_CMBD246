a5b5cce87992fe83fc27ba65f0f315baf00c69f8
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-06-12 11:42:08 +0600

    80% done

--- new.js
+++ new.js
@@ -1,41 +1,81 @@
 const generateBtnPin = document.querySelector('.generate-btn'); //button
-const generatePin =  document.querySelector('.generatedPin'); //input class generatedPin
+const aInput =  document.querySelector('.generatedPin'); //input class generatedPin
 
 /*1. Generating 4 digits Pin start */
 
-function generatePinResult(){
-    let randomNum = Math.floor(Math.random() * 9000 + 1000);
-    generatePin.value = randomNum;
+function resultGeneratePin (){
+    let randomNum = Math.floor(Math.random() *9000 + 1000);
+    aInput.value = randomNum;
 
 }
-generateBtnPin.addEventListener('click', generatePinResult);
+generateBtnPin.addEventListener('click', resultGeneratePin);
 
-/* Generating 4 digits Pin Ends */
+/* Ends */
 
 /*2. show value on the keypad start here*/
-const showValue =  document.querySelector('.showValue'); //input class showValue
+const bInput =  document.querySelector('.showValue'); //input class showValue
 function showKeyPadInput(call){
-    if(generatePin.value === "" ){
-        alert('please inter a PIN number');
+    if(aInput.value === "" ){
+        alert('please Enter a PIN number');
     }else{
-        showValue.value += call;
+        bInput.value += call;
     }
 
     // button C 
     if (call === 'C'){
-        showValue.value = '';
+        bInput.value = '';
     }
 }
 // remove digit '|<'
 function removeDigit(){
-    if(generatePin.value === "" ){
-        alert('please inter a PIN number');
+    if(aInput.value === "" ){
+        alert('please Enter a PIN number');
     }else{
-        let currentValue =  showValue.value;
-        showValue.value = currentValue.slice(0, -1);
+        let currentValue =  bInput.value;
+        bInput.value = currentValue.slice(0, -1);
         console.log(currentValue);
 
     }
 
 }
-/*show value on the keypad ends here*/
+/* Ends */
+
+/*3. check the pin is start here*/
+const submitBtn = document.querySelector('.submit-btn'); // submit bottn
+function resultSubmitBtn(){
+    let aPin = aInput.value;
+    let bPin = bInput.value;
+    if(aPin === bPin){
+        //console.log('congratulation');
+        showTrueNotification();
+    }else{
+        //console.log('Invalid');
+       showFalseNotification();
+    }
+}
+submitBtn.addEventListener('click', resultSubmitBtn);
+/* Ends */
+
+/*4  Make Notification Work */
+const wrongPinNotification = document.querySelector('.wrong-pin');
+const correctPinNotification = document.querySelector('.correct-pin');
+
+function hideNotification (){
+    wrongPinNotification.style.display = "none";
+    correctPinNotification.style.display = "none";
+}
+function showTrueNotification(){
+    correctPinNotification.style.display = "block";
+    wrongPinNotification.style.display = "none";
+}
+function showFalseNotification(){
+    wrongPinNotification.style.display = "block";
+    correctPinNotification.style.display = "none";
+
+}
+
+hideNotification();
+// showTrueNotification();
+// showFalseNotification();
+
+/* Ends */
